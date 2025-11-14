606c823de0e0ad1df15e3eebe1a3fe8c00bf3049
Frendy675 <frendyw7123@gmail.com>
2025-11-14 17:48:46 +0700

    Angular Lanjutan

--- griya-mdp/src/app/home/home.ts
+++ griya-mdp/src/app/home/home.ts
@@ -1,4 +1,3 @@
-
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LokasiPerumahan } from '../lokasi-perumahan/lokasi-perumahan';
@@ -21,35 +20,10 @@ export class Home {
       bedrooms: 3,
       bathrooms: 2,
       area: 120,
-      image: 'https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=600&h=400&fit=crop',
+      image: 'https://images.unsplash.com/photo-1568605114967-8130f3a36994',
       rating: 4.5,
       status: 'Available'
     },
-    {
-      id: 2,
-      name: 'Taman Indah Village',
-      location: 'Tangerang',
-      price: 650000000,
-      bedrooms: 2,
-      bathrooms: 1,
-      area: 90,
-      image: 'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?w=600&h=400&fit=crop',
-      rating: 4.8,
-      status: 'Available'
-    },
-    {
-      id: 3,
-      name: 'Villa Sejahtera',
-      location: 'Bogor',
-      price: 1200000000,
-      bedrooms: 4,
-      bathrooms: 3,
-      area: 180,
-      image: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?w=600&h=400&fit=crop',
-      rating: 4.9,
-      status: 'Available'
-    }
+    // ... data lainnya
   ];
-
-  filteredList = [];
 }
